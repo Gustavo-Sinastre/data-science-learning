f1b0e95a07fc37a078294c486b160d5655c2528a
LuisSinastre <luisgustavosinastre13@gmail.com>
2025-02-23 21:42:08 -0300

    feat: starting studies

---
+++ strings.py
@@ -0,0 +1,19 @@
+# Learning about strings
+
+# Verificando tamanho de um texto
+string = "caneta"
+size = len(string)
+print(size)
+
+newString = string.replace("c","C")
+print(string)
+print(newString)
+
+capitalizedString = string.capitalize()
+print(capitalizedString)
+
+upperString = string.upper()
+print(upperString)
+
+index = string.find("t")
+print(index)
